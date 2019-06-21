c2b206f2d52a78552ed752c2d08a66452d32c6dc
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2019-06-20 22:36:51 -0300

    Atualizando estilos

--- src/components/ChatListScreen/ChatNavbar/index.tsx
+++ src/components/ChatListScreen/ChatNavbar/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Toolbar } from '@material-ui/core';
+import styled from 'styled-components';
+
+const Container = styled(Toolbar)`
+  background-color: var(--primary-bg);
+  color: var(--primary-text);
+  font-size: 20px;
+  line-height: 40px;
+` as typeof Toolbar;
 
 const ChatNavbar: React.FC = () => (
-  <Toolbar>
+  <Container>
     <div>Whatsapp Clone</div>
-  </Toolbar>
+  </Container>
 );
 
 export default ChatNavbar;
--- src/components/ChatListScreen/index.tsx
+++ src/components/ChatListScreen/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ChatNavbar from './ChatNavbar';
 import ChatList from './ChatList';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  heigth: 100vh;
+`;
 
 const ChatListScreen: React.FC = () => (
-  <div>
+  <Container>
     <ChatNavbar />
     <ChatList />
-  </div>
+  </Container>
 );
 
 export default ChatListScreen;
